5c885b161901d928796516037da9695c7c84e98d
Obelich <obelich@gmail.com>
2013-08-05 11:24:13 -0700

    Starting the proyect template rail4 authentification

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TemplateRails4DiveCancanRolify::Application.config.secret_key_base = '9bf27dbe56c994152ff57eba830d2a688af9a9aafe2da7ab361a854934d52224329f6d0808fde8b8d321b0e4e2ccc6168a7c840d19ccf0d2412a2405a3250625'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TemplateRails4DiveCancanRolify::Application.config.session_store :cookie_store, key: '_template_Rails4_dive_cancan_rolify_session'
